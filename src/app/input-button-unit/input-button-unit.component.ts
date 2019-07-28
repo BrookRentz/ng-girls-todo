25d353dd8a2bad4eaf4ac2a4de8de5ff77faf8c4
Brook Rentz <brook.rentz@gmail.com>
2019-07-28 11:19:07 -0500

    initial commit

---
+++ src/app/input-button-unit/input-button-unit.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+
+@Component({
+  selector: 'todo-input-button-unit',
+  template: `
+    <input class="todo-input"
+          #inputElementRef
+          [value]="title"
+          (keyup.enter)="submitValue($event.target.value)">
+
+    <button class="btn"
+    (click)="submitValue(inputElementRef.value)">
+    Save
+    </button>
+  `,
+  styleUrls: ['./input-button-unit.component.css']
+})
+export class InputButtonUnitComponent implements OnInit {
+  title = 'Hello World';
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  submitValue(newTitle: string) {
+    this.submit.emit(newTitle);
+  }
+
+  @Output() submit: EventEmitter<string> = new EventEmitter();
+
+}
